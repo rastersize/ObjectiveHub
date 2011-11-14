7de75c8546a4d4504d45465f7f30e71704dc41bc
Aron Cedercrantz <aron@cedercrantz.se>
2011-11-14 11:43:26 +0100

    Fix API documentation generating script and target.

---
+++ Shared/FGOHError.h
@@ -0,0 +1,13 @@
+//
+//  FGOHError.h
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2011-11-14.
+//  Copyright (c) 2011 Fruit is Good. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGOHError : NSError
+
+@end
---
+++ Shared/FGOHError.m
@@ -0,0 +1,13 @@
+//
+//  FGOHError.m
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2011-11-14.
+//  Copyright (c) 2011 Fruit is Good. All rights reserved.
+//
+
+#import "FGOHError.h"
+
+@implementation FGOHError
+
+@end
