77b0b470d5dd8317feba01f6cdc66b5a769b4b76
Aron Cedercrantz <aron@cedercrantz.se>
2011-11-25 13:23:42 +0100

    Add CDOHRepository to the project.

---
+++ Shared/CDOHRepository.h
@@ -0,0 +1,13 @@
+//
+//  CDOHRepository.h
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2011-11-25.
+//  Copyright (c) 2011 Aron Cedercrantz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CDOHRepository : NSObject
+
+@end
---
+++ Shared/CDOHRepository.m
@@ -0,0 +1,13 @@
+//
+//  CDOHRepository.m
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2011-11-25.
+//  Copyright (c) 2011 Aron Cedercrantz. All rights reserved.
+//
+
+#import "CDOHRepository.h"
+
+@implementation CDOHRepository
+
+@end
