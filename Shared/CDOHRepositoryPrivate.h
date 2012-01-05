8e020d637c8edfbe630a5456420bc7c40ee47892
Aron Cedercrantz <aron@cedercrantz.se>
2012-01-05 21:12:28 +0100

    Add private header for CDOHRepository.

---
+++ Shared/CDOHRepositoryPrivate.h
@@ -0,0 +1,60 @@
+//
+//  CDOHRepositoryPrivate.h
+//  ObjectiveHub
+//
+//  Copyright 2011 Aron Cedercrantz. All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//  
+//  1. Redistributions of source code must retain the above copyright notice,
+//  this list of conditions and the following disclaimer.
+//  
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY ARON CEDERCRANTZ ''AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+//  EVENT SHALL ARON CEDERCRANTZ OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+//  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+//  The views and conclusions contained in the software and documentation are
+//  those of the authors and should not be interpreted as representing official
+//  policies, either expressed or implied, of Aron Cedercrantz.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark NSCoding and GitHub JSON Keys
+extern NSString *const kCDOHRepositoryHtmlUrlKey;
+extern NSString *const kCDOHRepositoryCloneUrlKey;
+extern NSString *const kCDOHRepositoryGitUrlKey;
+extern NSString *const kCDOHRepositorySshUrlKey;
+extern NSString *const kCDOHRepositorySvnUrlKey;
+extern NSString *const kCDOHRepositoryOwnerKey;
+extern NSString *const kCDOHRepositoryNameKey;	
+extern NSString *const kCDOHRepositoryDescriptionKey;
+extern NSString *const kCDOHRepositoryHomepageKey;
+extern NSString *const kCDOHRepositoryLanguageKey;
+extern NSString *const kCDOHRepositoryPrivateKey;
+extern NSString *const kCDOHRepositoryWatchersKey;
+extern NSString *const kCDOHRepositorySizeKey;
+extern NSString *const kCDOHRepositoryDefaultBranchKey;
+extern NSString *const kCDOHRepositoryOpenIssuesKey;
+extern NSString *const kCDOHRepositoryHasIssuesKey;
+extern NSString *const kCDOHRepositoryPushedAtKey;
+extern NSString *const kCDOHRepositoryCreatedAtKey;
+extern NSString *const kCDOHRepositoryOrganizationKey;
+extern NSString *const kCDOHRepositoryForkKey;
+extern NSString *const kCDOHRepositoryForksKey;
+extern NSString *const kCDOHRepositoryParentRepositoryKey;
+extern NSString *const kCDOHRepositorySourceRepositoryKey;
+extern NSString *const kCDOHRepositoryHasWikiKey;	
+extern NSString *const kCDOHRepositoryHasDownloadsKey;
