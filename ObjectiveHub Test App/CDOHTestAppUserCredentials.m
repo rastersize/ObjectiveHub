76123905d6f20c12266ed49a564807d3ccf7732b
Aron Cedercrantz <aron@cedercrantz.se>
2012-01-09 01:10:29 +0100

    Add account plist template file.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

---
+++ ObjectiveHub Test App/CDOHTestAppUserCredentials.m
@@ -0,0 +1,13 @@
+//
+//  CDOHTestAppUserCredentials.m
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2012-01-09.
+//  Copyright (c) 2012 Aron Cedercrantz. All rights reserved.
+//
+
+#import "CDOHTestAppUserCredentials.h"
+
+@implementation CDOHTestAppUserCredentials
+
+@end
