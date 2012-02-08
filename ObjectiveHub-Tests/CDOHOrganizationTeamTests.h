823f33dfd29a8ff7bb131898357baa7bf9e164eb
Aron Cedercrantz <aron@cedercrantz.se>
2012-02-08 18:40:59 +0100

    Add a bunch of (empty) unit tests file for resources.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

---
+++ ObjectiveHub-Tests/CDOHOrganizationTeamTests.h
@@ -0,0 +1,15 @@
+//
+//  CDOHOrganizationTeamTests.h
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2012-02-08.
+//  Copyright (c) 2012 Aron Cedercrantz. All rights reserved.
+//
+
+//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CDOHOrganizationTeamTests : SenTestCase
+
+@end
---
+++ ObjectiveHub-Tests/CDOHOrganizationTeamTests.m
@@ -0,0 +1,19 @@
+//
+//  CDOHOrganizationTeamTests.m
+//  ObjectiveHub
+//
+//  Created by Aron Cedercrantz on 2012-02-08.
+//  Copyright (c) 2012 Aron Cedercrantz. All rights reserved.
+//
+
+#import "CDOHOrganizationTeamTests.h"
+
+@implementation CDOHOrganizationTeamTests
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
