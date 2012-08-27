961752a9fbd88ac5a83943362878ea7a57f4015a
Aron Cedercrantz <aron@cedercrantz.se>
2012-08-27 03:38:10 +0200

    Add base 64 method to NSString.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

--- ObjectiveHub/NSString+ObjectiveHub.m
+++ ObjectiveHub/NSString+ObjectiveHub.m
@@ -38,11 +38,44 @@
 CDOH_FIX_CATEGORY_BUG(NSString_ObjectiveHub);
 @implementation NSString (ObjectiveHub)
 
+#pragma mark - Convert RFC 3339 Strings to Dates
 - (NSDate *)cdoh_dateUsingRFC3339Format
 {
 	NSDateFormatter *rfc3339DateFormatter = [NSDateFormatter cdoh_RFC3339DateFormatter];
 	NSDate *date = [rfc3339DateFormatter dateFromString:self];
 	return date;
+}
+
+
+#pragma mark - Base 64 Encoding
+- (NSString *)cdoh_base64String
+{
+	NSData *data = [NSData dataWithBytes:[self UTF8String] length:[self lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+    NSUInteger length = [data length];
+    NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    
+    uint8_t *input = (uint8_t *)[data bytes];
+    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
+    
+    for (NSUInteger i = 0; i < length; i += 3) {
+        NSUInteger value = 0;
+        for (NSUInteger j = i; j < (i + 3); j++) {
+            value <<= 8;
+            if (j < length) {
+                value |= (0xFF & input[j]);
+            }
+        }
+        
+        static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+        
+        NSUInteger idx = (i / 3) * 4;
+        output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
+        output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
+        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
+        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
+    }
+    
+    return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
 }
 
 @end
